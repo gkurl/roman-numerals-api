81095c70709ece9e53c09d547e141ee9b6c2cbaf
Gagandeep Kurl <gkurl@hotmail.co.uk>
2025-09-02 15:50:02 +0100

    Command implementation to test

--- app/Console/Commands/ConvertInteger.php
+++ app/Console/Commands/ConvertInteger.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use App\Services\ConversionService;
 use Illuminate\Console\Command;
+use App\Validation\ConversionValidationRules;
+use Illuminate\Support\Facades\Validator;
 
 class ConvertInteger extends Command
 {
@@ -34,8 +36,27 @@ class ConvertInteger extends Command
      */
     public function handle()
     {
-        $numberInput = $this->ask('Please enter a number between 1-3999)');
-        $output = $this->conversionService->convertAndRecord($numberInput);
-        $this->info('Your number was: ' . $numberInput . ' ' . 'In roman this is .' . $output);
+       $input = $this->argument('integer') ?? $this->ask('Enter your number (1-3999)');
+
+       $validator = Validator::make(
+           ['integer' => $input],
+           ConversionValidationRules::rules(),
+           ConversionValidationRules::messages()
+       );
+
+       if ($validator->fails()) {
+           $this->error('Invalid input: ' . $validator->errors()->first('integer'));
+           return 1;
+       }
+
+       try {
+           $integer = (int) $validator->validated()['integer'];
+           $convertedInteger = $this->conversionService->convertAndRecord($integer);
+           $this->info("$integer -> $convertedInteger");
+       } catch (\Exception $e) {
+            $this->error('Invalid input: ' . $e->getMessage());
+       }
+
+       return 0;
     }
 }
